471f3fa29b2700058dc879696f19b59d5a1effae
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2017-12-07 04:41:33 -0700

    data can be read now

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
+from scipy import signal
 from scipy.io import wavfile
 
 # referred from: https://www.kaggle.com/davids1992/data-visualization-and-investigation
@@ -19,14 +20,34 @@ def log_specgram(audio, sample_rate, window_size=20, step_size=10, eps=1e-10):
     return freqs, np.log(spec.T.astype(np.float32) + eps)
 
 
-def get_audio(type='train', path):
-	TRAIN_PATH = path + os.sep + 'train'
+def get_audio(path, type='train'):
+	TYPES = ['train', 'test', 'both']
+	ALLOWED_LABELS = ['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go', 'silence', 'unknown']
+	if type not in TYPES:
+		print("Argument type should be one of 'train', 'test', 'both'")
+		return 
+
+	TRAIN_PATH = path + os.sep + 'train' + os.sep + 'audio'
 	TEST_PATH = path + os.sep + 'test'
 	dataset_features = []
 	dataset_labels = []
 
 	if type == 'train':
-		print(os.listdir())
+		folders_list = os.listdir(TRAIN_PATH)
+		folders_list.remove('_background_noise_')
+
+		for folder in folders_list:
+			audio_files_list = os.listdir(TRAIN_PATH + os.sep + folder)
+
+			for audio_file in audio_files_list:
+				audio_file_path = TRAIN_PATH + os.sep + folder + os.sep + audio_file
+				samplerate, test_sound  = wavfile.read(audio_file_path)
+				_, spectrogram = log_specgram(test_sound, samplerate)
+				# print(spectrogram.shape)
+				# plt.imshow(spectrogram, aspect='auto', origin='lower')
+				# plt.show()
+
+
 
 
 
@@ -34,3 +55,6 @@ def get_audio(type='train', path):
 # samplerate, test_sound  = wavfile.read(filepath)
 # _, spectrogram = log_specgram(test_sound, samplerate)
 # plt.imshow(spectrogram.T, aspect='auto', origin='lower')
+
+TRAIN_PATH = 'G:/DL/tf_speech_recognition'
+get_audio(TRAIN_PATH, type='train')
