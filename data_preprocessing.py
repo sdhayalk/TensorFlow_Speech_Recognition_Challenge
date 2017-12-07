ea70bc639620878a42340ba4280ee4337224096b
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2017-12-07 05:56:12 -0700

    basic data preprocessing done, TODO: remove break so as to get all data and files

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -4,6 +4,14 @@ import matplotlib.pyplot as plt
 
 from scipy import signal
 from scipy.io import wavfile
+import statistics
+
+
+def get_silence_average(path):
+	path = path + os.sep + 'silence' + os.sep + '3e7124ba_nohash_0.wav'
+	samplerate, test_sound  = wavfile.read(path)
+	print(test_sound)
+
 
 # referred from: https://www.kaggle.com/davids1992/data-visualization-and-investigation
 def log_specgram(audio, sample_rate, window_size=20, step_size=10, eps=1e-10):
@@ -22,15 +30,23 @@ def log_specgram(audio, sample_rate, window_size=20, step_size=10, eps=1e-10):
 
 def get_audio(path, type='train'):
 	TYPES = ['train', 'test', 'both']
-	ALLOWED_LABELS = ['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go', 'silence', 'unknown']
 	if type not in TYPES:
 		print("Argument type should be one of 'train', 'test', 'both'")
 		return 
 
 	TRAIN_PATH = path + os.sep + 'train' + os.sep + 'audio'
 	TEST_PATH = path + os.sep + 'test'
+	ALLOWED_LABELS = ['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go', 'silence', 'unknown']
+	SILENCE_AVERAGE = get_silence_average(TRAIN_PATH)
 	dataset_features = []
 	dataset_labels = []
+
+	one_hot_map = {}
+	label_index = 0
+	for allowed_label in ALLOWED_LABELS:
+		one_hot_map[allowed_label] = label_index
+		label_index += 1
+
 
 	if type == 'train':
 		folders_list = os.listdir(TRAIN_PATH)
@@ -42,10 +58,32 @@ def get_audio(path, type='train'):
 			for audio_file in audio_files_list:
 				audio_file_path = TRAIN_PATH + os.sep + folder + os.sep + audio_file
 				samplerate, test_sound  = wavfile.read(audio_file_path)
+				
+				if len(test_sound) < 16000:
+					diff = 16000 - len(test_sound)
+					while(diff > 0):
+						test_sound = np.insert(test_sound, 1, 0)
+						diff -= 1
+				# print(len(test_sound))
+
 				_, spectrogram = log_specgram(test_sound, samplerate)
-				# print(spectrogram.shape)
-				# plt.imshow(spectrogram, aspect='auto', origin='lower')
-				# plt.show()
+
+				dataset_features.append(spectrogram.T)
+				if folder in ALLOWED_LABELS:
+					label_index = one_hot_map[folder]
+					label = np.zeros(len(ALLOWED_LABELS))
+					label[label_index] = 1
+					dataset_labels.append(label)
+				else:
+					label_index = one_hot_map['unknown']
+					label = np.zeros(len(ALLOWED_LABELS))
+					label[label_index] = 1
+					dataset_labels.append(label)
+
+				break
+
+	return np.array(dataset_features), np.array(dataset_labels)
+
 
 
 
@@ -57,4 +95,5 @@ def get_audio(path, type='train'):
 # plt.imshow(spectrogram.T, aspect='auto', origin='lower')
 
 TRAIN_PATH = 'G:/DL/tf_speech_recognition'
-get_audio(TRAIN_PATH, type='train')
+dataset_train_features, dataset_train_labels = get_audio(TRAIN_PATH, type='train')
+print(dataset_train_features.shape)
