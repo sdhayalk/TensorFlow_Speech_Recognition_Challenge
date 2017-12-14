02eda2170439def7834ef2f6ee2fc74bcdac8393
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2017-12-14 16:46:45 -0700

    implemented only RNN_LSTM

--- main_RNN_LSTM_tensorflow.py
+++ main_RNN_LSTM_tensorflow.py
@@ -4,6 +4,7 @@ import random
 
 # from tensorflow.python.ops import rnn
 from tensorflow.contrib import rnn
+from keras.layers import merge
 from data_preprocessing import get_audio_dataset_features_labels, get_audio_test_dataset_filenames, get_audio_test_dataset_features_labels, normalize_training_dataset, normalize_test_dataset
 
 def shuffle_randomize(dataset_features, dataset_labels):
@@ -14,11 +15,12 @@ def shuffle_randomize(dataset_features, dataset_labels):
 
 def get_batch(dataset, i, BATCH_SIZE):
 	if i*BATCH_SIZE+BATCH_SIZE > dataset.shape[0]:
-		return dataset[i*BATCH_SIZE:, :]
-	return dataset[i*BATCH_SIZE:(i*BATCH_SIZE+BATCH_SIZE), :]
+		return dataset[i*BATCH_SIZE:, :], dataset.shape[0] - i*BATCH_SIZE
+	return dataset[i*BATCH_SIZE:(i*BATCH_SIZE+BATCH_SIZE), :], BATCH_SIZE
 
 
-DATASET_PATH = 'G:/DL/tf_speech_recognition'
+#DATASET_PATH = 'G:/DL/tf_speech_recognition'
+DATASET_PATH = '/home/paperspace/tf_speech_recognition'
 ALLOWED_LABELS = ['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go', 'silence', 'unknown']
 ALLOWED_LABELS_MAP = {}
 for i in range(0, len(ALLOWED_LABELS)):
@@ -52,13 +54,10 @@ BATCH_SIZE = 32
 
 x = tf.placeholder(tf.float32, shape=[None, NUM_CHUNKS, CHUNK_SIZE])
 y = tf.placeholder(tf.float32, shape=[None, NUM_CLASSES])
+current_batch_size = tf.placeholder(tf.int32)
 
-def recurrent_neural_network(x):
-	print(x.shape)
-	# x = tf.transpose(x, [1,0,2])
-	# x = tf.reshape(x, [-1, CHUNK_SIZE])
-	# x = tf.split(x, NUM_CHUNKS, 0)
 
+def recurrent_neural_network(x, current_batch_size):
 	lstm_cell_1 = rnn.LSTMCell(128, state_is_tuple=True)
 	lstm_cell_2 = rnn.LSTMCell(256, state_is_tuple=True)
 	lstm_cell_3 = rnn.LSTMCell(384, state_is_tuple=True)
@@ -76,8 +75,9 @@ def recurrent_neural_network(x):
 	fully_connected_2 = tf.matmul(fully_connected_1, weights_2) + biases_2
 
 	return fully_connected_2
+	
 
-logits = recurrent_neural_network(x)
+logits = recurrent_neural_network(x, current_batch_size)
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
 optimizer = tf.train.AdamOptimizer()
 training = optimizer.minimize(loss)
@@ -90,51 +90,87 @@ with tf.Session() as sess:
 		total_cost = 0
 
 		for i in range(0, int(NUM_EXAMPLES/BATCH_SIZE)):
-			batch_x = get_batch(dataset_train_features, i, BATCH_SIZE)	# get batch of features of size BATCH_SIZE
-			batch_y = get_batch(dataset_train_labels, i, BATCH_SIZE)	# get batch of labels of size BATCH_SIZE
+			batch_x, batch_current_batch_size = get_batch(dataset_train_features, i, BATCH_SIZE)	# get batch of features of size BATCH_SIZE
+			batch_y, _ = get_batch(dataset_train_labels, i, BATCH_SIZE)	# get batch of labels of size BATCH_SIZE
 
-			_, batch_cost = sess.run([training, loss], feed_dict={x: batch_x, y: batch_y})	# train on the given batch size of features and labels
+			_, batch_cost = sess.run([training, loss], feed_dict={x: batch_x, y: batch_y, current_batch_size: batch_current_batch_size})	# train on the given batch size of features and labels
 			total_cost += batch_cost
 
 		print("Epoch:", epoch, "\tCost:", total_cost)
 
-		# # predict validation accuracy after every epoch
-		# y_predicted = tf.nn.softmax(logits)
-		# correct = tf.equal(tf.argmax(y_predicted, 1), tf.argmax(y, 1))
-		# accuracy_function = tf.reduce_mean(tf.cast(correct, 'float'))
-		# accuracy_validation = accuracy_function.eval({x:dataset_validation_features, y:dataset_validation_labels})
-		# print("Validation Accuracy in Epoch ", epoch, ":", accuracy_validation)
-		# # training end
+		# predict validation accuracy after every epoch
+		sum_accuracy_validation = 0.0
+		sum_i = 0
+		for i in range(0, int(dataset_validation_features.shape[0]/BATCH_SIZE)):
+			batch_x, batch_current_batch_size = get_batch(dataset_validation_features, i, BATCH_SIZE)
+			batch_y, _ = get_batch(dataset_validation_labels, i, BATCH_SIZE)
+			# print(batch_current_batch_size)
 
-		# # testing
-		# y_predicted_labels = []
-		# audio_files_list = []
-		# dataset_test_features = []
+			y_predicted = tf.nn.softmax(logits)
+			correct = tf.equal(tf.argmax(y_predicted, 1), tf.argmax(y, 1))
+			accuracy_function = tf.reduce_mean(tf.cast(correct, 'float'))
+			accuracy_validation = accuracy_function.eval({x:batch_x, y:batch_y, current_batch_size:batch_current_batch_size})
 
-		# for audio_file in audio_filenames:
-		# 	audio_files_list.append(audio_file)
-		# 	dataset_test_features.append(get_audio_test_dataset_features_labels(DATASET_PATH, audio_file))
+			sum_accuracy_validation += accuracy_validation
+			sum_i += 1
+			print("Validation Accuracy in Epoch ", epoch, ":", accuracy_validation, 'sum_i:', sum_i, 'sum_accuracy_validation:', sum_accuracy_validation)
+			# training end
 
-		# 	if len(audio_files_list) > 10000:
-		# 		audio_files_list = []
-		# 		dataset_test_features = np.array(dataset_test_features)
-		# 		dataset_test_features = normalize_test_dataset(dataset_test_features, min_value, max_value)
-		# 		y_predicted_labels.append(sess.run(tf.argmax(y_predicted, 1), feed_dict={x: dataset_test_features}))
+		# testing
+		if epoch > 0 and epoch%2 == 0:
+			y_predicted_labels = []
+			audio_files_list = []
+			dataset_test_features = []
+			test_samples_picked = 0
+			y_predicted = tf.nn.softmax(logits)
 
-		# # testing end
+			for audio_file in audio_filenames:
+				audio_files_list.append(audio_file)
+				dataset_test_features.append(get_audio_test_dataset_features_labels(DATASET_PATH, audio_file))
 
-		# # writing predicted labels into a csv file
-		# y_predicted_labels = np.array(y_predicted_labels)
-		# with open('run'+str(epoch)+'.csv','w') as file:	
-		# 	file.write('fname,label')
-		# 	file.write('\n')
+				if len(audio_files_list) == 3200:
+					dataset_test_features = np.array(dataset_test_features)
+					dataset_test_features = normalize_test_dataset(dataset_test_features, min_value, max_value)
 
-		# 	for i in range(0, y_predicted_labels.shape[0]):
-		# 		file.write(str(audio_filenames[i]) + ',' + str(ALLOWED_LABELS_MAP[str(int(y_predicted_labels[i]))]))
-		# 		file.write('\n')
+					for i in range(0, int(dataset_test_features.shape[0]/BATCH_SIZE)):
+						batch_x, batch_current_batch_size = get_batch(dataset_test_features, i, BATCH_SIZE)
+						temp = sess.run(tf.argmax(y_predicted, 1), feed_dict={x: batch_x, current_batch_size: batch_current_batch_size})
+						for element in temp:
+							y_predicted_labels.append(element) 
+
+					test_samples_picked += 3200
+					print('test_samples_picked:', test_samples_picked)
+
+					# writing predicted labels into a csv file
+					# y_predicted_labels = np.array(y_predicted_labels)
+					with open('run'+str(epoch)+'.csv','a') as file:	
+						for i in range(0, len(y_predicted_labels)):
+							file.write(str(audio_files_list[i]) + ',' + str(ALLOWED_LABELS_MAP[str(int(y_predicted_labels[i]))]))
+							file.write('\n')
+
+					y_predicted_labels = []
+					dataset_test_features = []
+					audio_files_list = []
+
+			# last set
+			dataset_test_features = np.array(dataset_test_features)
+			dataset_test_features = normalize_test_dataset(dataset_test_features, min_value, max_value)
+
+			for i in range(0, int(dataset_test_features.shape[0]/BATCH_SIZE)):
+				batch_x, batch_current_batch_size = get_batch(dataset_test_features, i, BATCH_SIZE)
+				temp = sess.run(tf.argmax(y_predicted, 1), feed_dict={x: batch_x, current_batch_size: batch_current_batch_size})
+				for element in temp:
+					y_predicted_labels.append(element) 
+
+			test_samples_picked += 3200
+			print('test_samples_picked:', test_samples_picked)
+
+			# writing predicted labels into a csv file
+			# y_predicted_labels = np.array(y_predicted_labels)
+			with open('run'+str(epoch)+'.csv','a') as file:	
+				for i in range(0, len(y_predicted_labels)):
+					file.write(str(audio_files_list[i]) + ',' + str(ALLOWED_LABELS_MAP[str(int(y_predicted_labels[i]))]))
+					file.write('\n')
+
 
 		
-
-
-
-
